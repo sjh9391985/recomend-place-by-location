7f73ab6e3b5c5a723f32f076b6fc3e816439db89
sjh9391985 <sjh9391985@gmail.com>
2024-02-15 20:47:09 +0900

    feat: "Kakao 조회 URI 추가"

---
+++ src/main/java/dev/be/project/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,25 @@
+package dev.be.project.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI buildUriByAddressSearch(String address) {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        uriComponentsBuilder.queryParam("query", address);
+
+        URI uri = uriComponentsBuilder.build().encode().toUri();
+        log.info("[Kakao URI Service] : {}", uri);
+
+        return uri;
+    }
+
+}
