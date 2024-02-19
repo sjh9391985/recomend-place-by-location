a9428dab5f5c9ff66b76f12abd6bbdde1993dee9
sjh9391985 <sjh9391985@gmail.com>
2024-02-19 22:14:08 +0900

    feat: "약국 테스트 추가"

---
+++ src/test/groovy/dev/be/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -0,0 +1,38 @@
+package dev.be.project.pharmacy.repository
+
+import dev.be.project.AbstractIntegrationContainerBaseTest
+import dev.be.project.pharmacy.entity.Pharmacy
+import org.springframework.beans.factory.annotation.Autowired
+
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
+    def "PharmacyRepository save" () {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+        .pharmacyAddress(address)
+        .pharmacyName(name)
+        .latitude(latitude)
+        .longitude(longitude)
+        .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLatitude() == latitude
+        result.longitude == longitude
+
+    }
+
+
+}
