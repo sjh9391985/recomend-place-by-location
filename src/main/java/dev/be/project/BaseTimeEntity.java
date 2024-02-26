11a016750e9e3c06ce7d66e95e2db0e66d6b09ef
sjh9391985 <sjh9391985@gmail.com>
2024-02-26 21:59:47 +0900

    feat: "BaseEntity 추가"

---
+++ src/main/java/dev/be/project/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package dev.be.project;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false) // 명시적으로 업데이트 되지 말아야할 정보
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
