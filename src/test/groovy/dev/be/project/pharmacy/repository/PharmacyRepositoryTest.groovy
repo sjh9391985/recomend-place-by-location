d303985e5ed36e1b87d02838d7b3601f0422ddf7
sjh9391985 <sjh9391985@gmail.com>
2024-02-26 22:00:56 +0900

    test: "dirty check 테스트코드"

--- src/test/groovy/dev/be/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/dev/be/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,6 +4,8 @@ import dev.be.project.AbstractIntegrationContainerBaseTest
 import dev.be.project.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
 
     @Autowired
@@ -58,6 +60,30 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest{
 
         then:
         result.size() == 1
+    }
+
+    def "BaseTimeEntity 등록" () {
+
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+        .pharmacyAddress(address)
+        .pharmacyName(name)
+        .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate()
+                .isAfter(now) // now 시간보다 최근인지 검증하는 메서드
+
+
+
     }
 
 
