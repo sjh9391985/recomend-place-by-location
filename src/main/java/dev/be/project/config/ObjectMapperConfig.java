b7754519df3773bc34cdc1820cf4de71f05f324a
sjh9391985 <sjh9391985@gmail.com>
2024-03-09 14:15:24 +0900

    feat: "objectmapper config 빈등록"

---
+++ src/main/java/dev/be/project/config/ObjectMapperConfig.java
@@ -0,0 +1,21 @@
+package dev.be.project.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+}
